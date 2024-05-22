d845d39f72d2a6b88a1100ee3436ab9f7b5b51d9
florinivan <41228455+florinivan@users.noreply.github.com>
2024-05-22 20:51:04 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,21 +1,24 @@
 var fruits= ['apple','banana','mango','ananas'];
 var pos= fruits.indexOf ('mango');
 
-/**Ciclo For:
+/**
+Crea un nuovo branch e fai i seguenti esercizi
 
+* Ciclo For:
 Scrivi un esempio di un ciclo for che stampa i numeri da 1 a 5.
-Ciclo While:
 
+* Ciclo While:
 Qual è la differenza tra un ciclo for e un ciclo while?
 Scrivi un esempio di un ciclo while che stampa i numeri da 1 a 5.
-Istruzione Condizionale:
 
+* Istruzione Condizionale:
 Spiega la differenza tra l'istruzione "if" e l'istruzione "else if" in JavaScript.
 Scrivi un esempio di un'istruzione condizionale che verifica se un numero è pari o dispari.
-Ciclo For...of:
 
+* Ciclo For...of:
 Qual è lo scopo del ciclo for...of in JavaScript?
 Scrivi un esempio di un ciclo for...of che itera su un array di nomi e li stampa.
 
-Esercizio: Scrivi una funzione in JavaScript chiamata "printEvenNumbers" che accetta un numero intero positivo come argomento e stampa tutti i numeri pari da 1 fino al numero specificato. Utilizza un ciclo for per risolvere questo problema.
+* Esercizio: Scrivi una funzione in JavaScript chiamata "printEvenNumbers" che accetta un numero intero positivo come argomento e stampa tutti i numeri pari da 1 fino al numero specificato. Utilizza un ciclo for per risolvere questo problema.
+
 **/
